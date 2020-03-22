10d219be198a09f7907162276dd4a78ef54e7fdd
John Michaelsen <john.michaelsen@gmail.com>
2020-03-22 19:39:26 +0100

    Prepared click-handling and functions for dealing cards

--- poker.js
+++ poker.js
@@ -4,3 +4,27 @@ getNewDeck = function(){
 	deck = doc.randomizedDeck();
 	return deck;
 }
+
+
+setNextDealerAndDealHand = function() {
+	console.log("setting dealer and dealing new hand")
+
+}
+
+
+
+showFlop = function() {
+	console.log("showing flop")
+
+}
+
+showTurn = function() {
+	console.log("showing turn")
+
+}
+
+
+showRiver = function() {
+	console.log("showing river")
+
+}
--- public/bundle.js
+++ public/bundle.js
@@ -85,4 +85,28 @@ getNewDeck = function(){
 	deck = doc.randomizedDeck();
 	return deck;
 }
+
+
+setNextDealerAndDealHand = function() {
+	console.log("setting dealer and dealing new hand")
+
+}
+
+
+
+showFlop = function() {
+	console.log("showing flop")
+
+}
+
+showTurn = function() {
+	console.log("showing turn")
+
+}
+
+
+showRiver = function() {
+	console.log("showing river")
+
+}
 },{"deck-o-cards":1}]},{},[2]);
