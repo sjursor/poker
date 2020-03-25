d572f66d341cf1e5d2e2494c563d2302af25b74c
Sjur Sørskår <sjurgran@gmail.com>
2020-03-25 21:23:30 +0100

    ssl

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var		firebase = require('firebase-admin');
 const 	express = require('express');
 const 	ecstatic = require('ecstatic');
 const 	http = require('http');
+const 	https = require('https');
+const	fs = require('fs');
 
-const app = express();
+var privateKey = fs.readFileSync('sslcert/localhost.key');
+var certificate = fs.readFileSync('sslcert/localhost.crt');
+var credentials = {key: privateKey, cert: certificate};
+
+var app = express();
 
 app.use('/scripts', express.static(__dirname + '/node_modules/'));
 //app.use('/firebase.js', express.static(__dirname + 'firebase.js'));
@@ -14,6 +20,13 @@ app.use(ecstatic({
   showdir: false,
 }));
 
-http.createServer(app).listen(8080);
+//var httpServer = http.createServer(app);
+var httpsServer = https.createServer(credentials, app);
+
+//httpServer.listen(8080);
+httpsServer.listen(8443);
+
+//http.createServer(app).listen(8080);
 
 console.log('See if its cool on -> :8080');
+console.log('See if its https on -> :8443');
