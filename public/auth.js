38223b82322700eb43a6c53c1c59436b343ec087
Sjur Sørskår <sjurgran@gmail.com>
2020-03-26 21:37:40 +0100

    graps

--- public/auth.js
+++ public/auth.js
@@ -1,19 +1,49 @@
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '171188903858338',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v6.0'
-    });
-      
-    FB.AppEvents.logPageView();   
-      
-  };
+loginHandler = function(){
+  var provider = new firebase.auth.FacebookAuthProvider();
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      console.log("user is signed in");
+      showGame(user);
+    } else {
+      firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        newPlayer(user.uid, user.email, user.displayName, user.photoURL, function(pid){
+          currentPlayer = pid;
+          console.log("Current room "+currentRoom);
+          addPlayerToTable(pid,currentRoom);
+          jQuery("#userinfo").text("User: "+user.displayName);
+        });
+        showGame();
+
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        console.log(errorMessage, errorCode, email);
+        // ...
+      });
+    }
+  });  
+}
+
+
+const firebaseConfig = {
+        apiKey: "AIzaSyDl7d1_BOmwKUCKE22PjSBGagUsXtIy9EU",
+        authDomain: "poker-9c76f.firebaseapp.com",
+        databaseURL: "https://poker-9c76f.firebaseio.com",
+        projectId: "poker-9c76f",
+        storageBucket: "poker-9c76f.appspot.com",
+        messagingSenderId: "661582615603",
+        appId: "1:661582615603:web:bdbbe82e9fd68d265b3f46"
+      };
+
+      firebase.initializeApp(firebaseConfig);
